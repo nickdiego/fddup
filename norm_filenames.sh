34c03793d755949269b4747c99baaa3dfce7fa2e
Nick Diego Yamane <nickdiego@igalia.com>
2020-04-07 23:18:46 -0400

    Fix file name normalization and printing
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- norm_filenames.sh
+++ norm_filenames.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# Usage sample:
+# Usage:
+
+# Dry-run:
+# ./norm_filenames.sh <dir> [<dir2>...<dirn>]
 #
-# find test/odd-filenames -type f | ./norm_filenames.sh
+# Rename files:
+# dry=0 ./norm_filenames.sh <dir> [<dir2>...<dirn>]
 
 regex='s,^.\+\(Aula .\+\.mp4\).\+$,\1,g'
 dry=${dry:-1}
 
-while IFS= read -r f; do
+while IFS= read -r -d $'\0' f; do
   dir="$(dirname "$f")"
-  new_name="$(echo "$f" | sed "$regex")"
-  cmd="mv '$f' '${dir}/${new_name}'"
-
-  (( dry )) && echo $cmd || eval $cmd
-done
+  new_name="${dir}/$(echo "$f" | sed "$regex" | tr ' ' '_')"
+  cmd="mv $(printf "%q\n" "$f") '${new_name}'"
+  echo "-> '$new_name'"
+  (( dry )) || eval $cmd
+done < <(find "$@" -type f -print0)
 
 # vim: ts=2 sw=4 et filetype=sh
 
